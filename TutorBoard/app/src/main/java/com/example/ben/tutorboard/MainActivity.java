ec3ed548bc6f4d07bf80f55cb7ff29daf2bfe7c1
Ben <ben13castillo@yahoo.com>
2018-03-01 20:50:15 -0800

    Added Login and Buttons

--- TutorBoard/app/src/main/java/com/example/ben/tutorboard/MainActivity.java
+++ TutorBoard/app/src/main/java/com/example/ben/tutorboard/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.ben.tutorboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Sends the user to the login screen from the "Create" button
+        buttonLog = (Button) findViewById(R.id.buttonLog);
+        buttonLog.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+    }
+
+    public void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
