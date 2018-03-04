a5b3e64bce6125641839be9e78031bb8811d1237
Agustin Barajas <agustincards14@gmail.com>
2018-03-03 22:08:57 -0800

    Added back button to EnterSession activity
    And "cancel button" goes back to parent activity AKA finishes EnterSession activity

--- TutorBoard/app/src/main/java/com/example/ben/tutorboard/EnterSession.java
+++ TutorBoard/app/src/main/java/com/example/ben/tutorboard/EnterSession.java
@@ -1,13 +1,48 @@
 package com.example.ben.tutorboard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class EnterSession extends AppCompatActivity {
+
+   private Button cancelButton;
+   private Button joinButton;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_enter_session);
+
+      //Sends the user to the login screen from the "Create" button
+      cancelButton = (Button) findViewById(R.id.cancelJoinButton);
+      cancelButton.setOnClickListener(new View.OnClickListener(){
+         @Override
+         public void onClick(View v) {
+            backToHome();
+         }
+      });
+
+      //Click Join button -> EnterSession screen
+      joinButton = (Button)findViewById(R.id.joinButton);
+      joinButton.setOnClickListener(new View.OnClickListener(){
+         @Override
+         public void onClick(View v) {
+            Toast.makeText(EnterSession.this, "'Join session' coming soon!", Toast.LENGTH_SHORT).show();
+            enterSession();
+         }
+      });
    }
+
+   public void backToHome(){
+      finish();
+   }
+
+   public void enterSession(){
+
+   }
+
+
 }
